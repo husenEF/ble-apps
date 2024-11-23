773e1b011d7c7395dede292bafdc84960c143021
Husen Efendi <husenef@gmail.com>
2024-11-23 12:31:02 +0700

    update custom hook documentation

--- src/service/useBleConnect.ts
+++ src/service/useBleConnect.ts
@@ -2,12 +2,15 @@ import {useEffect, useState} from 'react';
 import {Platform, PermissionsAndroid, Alert, Linking} from 'react-native';
 import {BleManager, Device} from 'react-native-ble-plx';
 
-interface PropConnect {
+interface BleState {
   data: Device[];
   connectedDeviceItems: Device[];
   isScanning: boolean;
   bluetoothState: string | null;
   isBluetoothReady: boolean;
+}
+
+interface BleActions {
   requestPermissions: () => Promise<boolean>;
   startScanning: () => void;
   stopScanning: () => void;
@@ -16,6 +19,32 @@ interface PropConnect {
   waitUntilBluetoothReady: () => Promise<boolean>;
 }
 
+interface PropConnect extends BleState, BleActions {}
+
+/**
+ * useConnect is a React Hook that provides an interface to connect to nearby Bluetooth Low Energy (BLE) devices using the react-native-ble-plx library.
+ *
+ * The hook returns an object with the following properties:
+ *
+ * - `data`: An array of Device objects representing the devices that are currently scanning.
+ * - `connectedDeviceItems`: An array of Device objects representing the devices that are currently connected.
+ * - `isScanning`: A boolean indicating whether the device is currently scanning for BLE devices.
+ * - `requestPermissions`: A function that requests the necessary permissions to use BLE on the device.
+ * - `startScanning`: A function that starts scanning for BLE devices.
+ * - `stopScanning`: A function that stops scanning for BLE devices.
+ * - `connectToDevice`: A function that connects to a specific BLE device.
+ * - `disconnectFromDevice`: A function that disconnects from a specific BLE device.
+ * - `waitUntilBluetoothReady`: A function that waits until the device's Bluetooth adapter is ready.
+ * - `bluetoothState`: A string indicating the current state of the device's Bluetooth adapter.
+ * - `isBluetoothReady`: A boolean indicating whether the device's Bluetooth adapter is ready.
+ *
+ * The hook also handles the following side effects:
+ *
+ * - Requests the necessary permissions to use BLE on the device when the component mounts.
+ * - Starts scanning for BLE devices when the component mounts.
+ * - Stops scanning for BLE devices when the component unmounts.
+ * - Updates the `data` and `connectedDeviceItems` arrays when the device state changes.
+ */
 const useConnect = (): PropConnect => {
   const [manager] = useState<BleManager>(new BleManager());
   const [isScanning, setIsScanning] = useState<boolean>(false);
@@ -65,6 +94,13 @@ const useConnect = (): PropConnect => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  /**
+   * Request the necessary permissions to use BLE on Android.
+   * On Android 12 and above, it requests BLUETOOTH_SCAN and BLUETOOTH_CONNECT.
+   * On Android 11 and below, it requests ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION.
+   * On iOS, it simply returns true.
+   * @return {boolean} true if all the necessary permissions are granted.
+   */
   const requestPermissions = async (): Promise<boolean> => {
     if (Platform.OS === 'android') {
       const permissions =
@@ -90,6 +126,14 @@ const useConnect = (): PropConnect => {
     return true;
   };
 
+  /**
+   * @name stopScanning
+   * @returns {void}
+   * @description
+   * Stops the BLE device scan.
+   * If the device scan is not active, it does nothing.
+   * It also sets the isScanning state to false.
+   */
   const stopScanning = () => {
     if (!isScanning) {
       console.log('Not scanning');
@@ -100,6 +144,17 @@ const useConnect = (): PropConnect => {
     setIsScanning(false);
   };
 
+  /**
+   * @name waitUntilBluetoothReady
+   * @description
+   * Waits until the Bluetooth adapter is ready by monitoring its state.
+   * This function returns a Promise that resolves to true when the Bluetooth
+   * state becomes "PoweredOn". If the Bluetooth state changes to "Unauthorized",
+   * "Unsupported", or "PoweredOff", the Promise is rejected with an error.
+   *
+   * @returns {Promise<boolean>} A promise that resolves to true when Bluetooth is ready,
+   * or rejects with an error if the state is not suitable.
+   */
   const waitUntilBluetoothReady = async (): Promise<boolean> => {
     return new Promise((resolve, reject) => {
       const subscriptionState = manager.onStateChange(state => {
@@ -119,6 +174,18 @@ const useConnect = (): PropConnect => {
     });
   };
 
+/**
+ * @name startScanning
+ * @description
+ * Initiates the scanning process for BLE devices. If a scan is already in progress,
+ * the function logs a message and returns early. Otherwise, it starts a new scan,
+ * resets the current device data, and updates the scanning state.
+ *
+ * During scanning, discovered devices are added to the data state if they haven't been
+ * previously encountered. If an error occurs during scanning, the scanning process is stopped.
+ *
+ * @returns {Promise<void>} A promise that resolves when the scanning process is successfully initiated.
+ */
   const startScanning = async (): Promise<void> => {
     if (isScanning) {
       console.log('Already scanning');
@@ -150,6 +217,16 @@ const useConnect = (): PropConnect => {
     );
   };
 
+  /**
+   * @name connectToDevice
+   * @description
+   * Connects to a BLE device. Stops scan if in progress. Logs errors
+   * and connected devices.
+   *
+   * @param {Device} device The BLE device to connect to.
+   * @returns {Promise<void>} A promise that resolves when the device is
+   * successfully connected.
+   */
   const connectToDevice = async (device: Device): Promise<void> => {
     try {
       if (isScanning) {
@@ -183,6 +260,15 @@ const useConnect = (): PropConnect => {
     }
   };
 
+  /**
+   * @name disconnectFromDevice
+   * @description Disconnects from a connected BLE device. Logs errors and disconnected
+   * devices.
+   *
+   * @param {Device} device The BLE device to disconnect from.
+   * @returns {Promise<void>} A promise that resolves when the device is
+   * successfully disconnected.
+   */
   const disconnectFromDevice = async (device: Device): Promise<void> => {
     try {
       const isConnected = await manager.isDeviceConnected(device.id);
@@ -198,6 +284,15 @@ const useConnect = (): PropConnect => {
     }
   };
 
+  /**
+   * @name getAllDeviceConnected
+   * @description Fetches all BLE devices that are currently connected to the device.
+   * If serviceUUIDs are provided, only devices connected to those services are returned.
+   *
+   * @param {string[]} [serviceUUIDs=[]] The UUIDs of services whose connected devices to fetch.
+   * @returns {Promise<Device[]>} A promise that resolves to an array of connected BLE devices.
+   * @todo double check for this method
+   */
   const getAllDeviceConnected = async (
     serviceUUIDs: string[] = [],
   ): Promise<Device[]> => {
